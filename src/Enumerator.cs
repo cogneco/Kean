1ef7911592516c1ff7c95bbb99742a84af344242
Simon Mika <simon@mika.se>
2017-02-07 17:30:24 +0100

    Added Enumerator wrapper.

--- src/Enumerator.cs
+++ src/Enumerator.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2012  Simon Mika <simon@mika.se>
+// Copyright (C) 2012, 2016  Simon Mika <simon@mika.se>
 //
 // This file is part of Kean.
 //
@@ -16,12 +16,40 @@
 // along with Kean.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System;
 using Generic = System.Collections.Generic;
+using Kean.Extension;
 
 namespace Kean
 {
 	public static class Enumerator
 	{
 		public static Generic.IEnumerator<T> Empty<T>() { return Enumerable.Empty<T>().GetEnumerator(); }
+	}
+	public class Enumerator<T> :
+		Generic.IEnumerator<T>
+	{
+		Func<T> next;
+		Action reset;
+		Action dispose;
+		T current;
+		public T Current { get { return this.current; } }
+		object System.Collections.IEnumerator.Current { get { return this.current; } }
+		public Enumerator(Func<T> next, Action reset = null, Action dispose = null)
+		{
+			this.next = next;
+		}
+		public bool MoveNext()
+		{
+			return (this.current = this.next()).NotNull();
+		}
+		public void Reset()
+		{
+			this.reset.Call();
+		}
+		public void Dispose()
+		{
+			this.dispose.Call();
+		}
 	}
 }
