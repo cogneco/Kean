fc30de7b1d2cb1d62921b8124258543e3677aca7
Simon Mika <simon@mika.se>
2017-04-01 09:46:14 +0200

    Trying some obscure parameter.

--- myget.bat
+++ myget.bat
@@ -1,29 +1,29 @@
 REM @echo Off
 set config=%1
 if "%config%" == "" (
-   set config=Release
+	 set config=Release
 )
 
 set version=
 if not "%BuildCounter%" == "" (
-   set version=--version-suffix ci-%BuildCounter%
+	 set version=--version-suffix ci-%BuildCounter%
 )
 
 REM Restore
-call dotnet restore
+call dotnet --fx-version 1.0.0-preview2-1-003155 restore
 if not "%errorlevel%"=="0" goto failure
 
 REM Build
-call dotnet build src\ --configuration %config%
+call dotnet --fx-version 1.0.0-preview2-1-003155 build src\ --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-call dotnet test test\ --configuration %config%
+call dotnet --fx-version 1.0.0-preview2-1-003155 test test\ --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir %cd%\Artifacts
-call dotnet pack src\ --configuration %config% %version% --output Artifacts
+call dotnet --fx-version 1.0.0-preview2-1-003155 pack src\ --configuration %config% %version% --output Artifacts
 if not "%errorlevel%"=="0" goto failure
 
 :success
